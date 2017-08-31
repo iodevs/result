5a733638c6c3051de8d1602ed26012d33ddbac18
Jindrich K. Smitka <jsmitka@smita.info>
2017-08-31 14:52:17 +0200

    Added public API

--- lib/result.ex
+++ lib/result.ex
@@ -3,5 +3,13 @@ defmodule Result do
   Documentation for Result.
   """
 
+  defdelegate ok(value), to: Result.Ok, as: :of
+  defdelegate error(value), to: Result.Error, as: :of
+
   defdelegate map(result, f), to: Result.Operators
+  defdelegate perform(result, f), to: Result.Operators
+  defdelegate with_default(result, default), to: Result.Operators
+
+  defdelegate error?(result), to: Result.Operators
+  defdelegate ok?(result), to: Result.Operators
 end
