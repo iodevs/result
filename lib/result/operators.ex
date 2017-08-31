ae4c43782e8aebd91e6f74317f06a4f0c166d150
Jindrich K. Smitka <jsmitka@smita.info>
2017-08-31 14:51:57 +0200

    Added result check function

--- lib/result/operators.ex
+++ lib/result/operators.ex
@@ -1,7 +1,6 @@
 defmodule Result.Operators do
   @moduledoc """
   A result operators.
-
   """
 
   @doc """
@@ -16,6 +15,7 @@ defmodule Result.Operators do
     iex> error = {:error, 3}
     iex> Result.Operators.map(error, fn(x) -> x + 10 end)
     {:error, 3}
+
   """
   def map({:ok, value}, f) do
     {:ok, f.(value)}
@@ -33,12 +33,12 @@ defmodule Result.Operators do
 
     iex> Result.Operators.perform({:error, 123}, fn(x) -> IO.puts(x) end)
     {:error, 123}
+
   """
   def perform({:ok, value} = result, f) do
     f.(value)
     result
   end
-
   def perform({:error, _} = result, _f), do: result
 
   @doc """
@@ -51,7 +51,38 @@ defmodule Result.Operators do
 
     iex> Result.Operators.with_default({:error, 123}, 456)
     456
+
   """
   def with_default({:ok, value}, _default), do: value
   def with_default({:error, _}, default), do: default
+
+  @doc """
+  Return `true` if result is error
+
+  ## Examples
+
+    iex> Result.Operators.error?({:error, 123})
+    true
+
+    iex> Result.Operators.error?({:ok, 123})
+    false
+
+  """
+  def error?({:error, _}), do: true
+  def error?(_result), do: false
+
+  @doc """
+  Return `true` if result is ok
+
+  ## Examples
+
+    iex> Result.Operators.ok?({:ok, 123})
+    true
+
+    iex> Result.Operators.ok?({:error, 123})
+    false
+
+  """
+  def ok?({:ok, _}), do: true
+  def ok?(_result), do: false
 end
