57c23353f32cf673409554a9b9bed5c2439c1dff
Jindrich K. Smitka <jsmitka@smita.info>
2017-09-26 16:34:15 +0200

    Bump version: 0.5.0 → 0.5.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Result.Mixfile do
   def project do
     [
       app: :result,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       test_coverage: [tool: ExCoveralls],
