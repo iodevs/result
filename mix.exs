3ef1ec0ba0220f78a857b2a19a965dd826de6618
Jindrich K. Smitka <jsmitka@smita.info>
2018-12-03 14:11:26 +0100

    Bump version: 1.2.0 → 1.2.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Result.Mixfile do
     [
       app: :result,
       dialyzer: dialyzer_base() |> dialyzer_ptl(System.get_env("SEMAPHORE_CACHE_DIR")),
-      version: "1.2.0",
+      version: "1.2.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
