aefeddeab46189a78eb46361c4d999fb05d1e543
Jindrich K. Smitka <jsmitka@smita.info>
2020-05-30 07:06:44 +0200

    Fixed code format

--- test/operators_test.exs
+++ test/operators_test.exs
@@ -9,7 +9,7 @@ defmodule ResultOperatorTest do
   describe "Perform" do
     test "run function if result is ok" do
       fun = fn ->
-        assert Operators.perform({:ok, 123}, fn(x) -> IO.puts(x) end) == {:ok, 123}
+        assert Operators.perform({:ok, 123}, fn x -> IO.puts(x) end) == {:ok, 123}
       end
 
       assert capture_io(fun) == "123\n"
@@ -17,11 +17,10 @@ defmodule ResultOperatorTest do
 
     test "don\'t run function if result is error" do
       fun = fn ->
-        assert Operators.perform({:error, 123}, fn(x) -> IO.puts(x) end) == {:error, 123}
+        assert Operators.perform({:error, 123}, fn x -> IO.puts(x) end) == {:error, 123}
       end
 
       assert capture_io(fun) == ""
     end
   end
 end
-
